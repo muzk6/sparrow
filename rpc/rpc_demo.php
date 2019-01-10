a26c3f698f14437a866242dd6d4264647ec741cb
yangjunping <yangjunping@115.com>
2019-01-10 14:58:08 +0800

    yar

---
+++ rpc/rpc_demo.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__DIR__) . '/boot/init.php';
+
+var_dump(extension_loaded('yar'));exit;
+class Foo
+{
+    public function bar($parameter, $option = "foo")
+    {
+        return [$parameter, $option];
+    }
+}
+
+$service = new Yar_Server(new Foo());
+$service->handle();
