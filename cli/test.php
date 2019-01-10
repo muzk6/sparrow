3e29802e6dda46667149bd09a8c2b5bf8cf6d029
muzk6 <muzk6x@gmail.com>
2019-01-10 23:53:43 +0800

    yar

--- cli/test.php
+++ cli/test.php
@@ -2,6 +2,3 @@
 
 require_once dirname(__DIR__) . '/boot/init.php';
 
-$client = new Yar_Client('http://localhost/knf/rpc/rpc_demo.php');
-$rs = $client->bar($_SERVER, 'hello');
-var_dump($rs);
--- rpc/rpc_demo.php
+++ rpc/rpc_demo.php
@@ -2,7 +2,6 @@
 
 require_once dirname(__DIR__) . '/boot/init.php';
 
-//var_dump(extension_loaded('yar'));exit;
 class Foo
 {
     public function bar($parameter, $option = "foo")
