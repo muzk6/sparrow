325d655b199077550b5772bc974887a5a9e032a7
muzk6 <muzk6x@gmail.com>
2019-01-11 21:07:03 +0800

    aes 对称加密

--- core/helpers.php
+++ core/helpers.php
@@ -192,3 +192,39 @@ function app_consume($queue, callable $callback)
     $channel->close();
     $connection->close();
 }
+
+/**
+ * aes 对称加密
+ * @param string $plainText 明文
+ * @return array
+ */
+function app_aes_encrypt($plainText)
+{
+    $method = 'aes-256-cbc';
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method), $isStrong);
+
+    $key = md5(uniqid());
+    $cipher = openssl_encrypt($plainText, $method, $key, 0, $iv);
+
+    return [
+        'cipher' => $cipher,
+        'iv' => $iv,
+        'key' => $key,
+        'is_strong' => $isStrong,
+    ];
+}
+
+/**
+ * aes 对称解密
+ * @param string $cipher base64格式的密文
+ * @param string $iv 分组加密的初始向量
+ * @param string $key 密钥
+ * @return string 密文
+ */
+function app_aes_decrypt($cipher, $iv, $key)
+{
+    $method = 'aes-256-cbc';
+    $plainText = openssl_decrypt($cipher, $method, $key, 0, $iv);
+
+    return $plainText;
+}
