99bbeb957226d5bcffc7dae71bd2bd777e31b288
yangjunping <yangjunping@115.com>
2019-01-11 12:55:52 +0800

    维护模式

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
 
+// 维护模式检查
+if (is_file(dirname(__DIR__). '/data/down')) {
+    http_response_code(403);
+    exit;
+}
+
 require_once dirname(__DIR__) . '/boot/init.php';
 require_once PATH_ROOT . '/boot/routes.php';
