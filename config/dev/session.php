8283ee8a6c5599e23de9b6494804e1b0b463d2bc
muzk6 <muzk6x@gmail.com>
2019-01-12 20:00:48 +0800

    session.name

--- config/dev/session.php
+++ config/dev/session.php
@@ -8,5 +8,6 @@ return [
 
     'session.gc_maxlifetime' => 1440, // session过期时间
     'session.cookie_lifetime' => 0, // cookie过期时间，0表示浏览器重启后失效
+    'session.name' => 'user_session',
     'session.cookie_httponly' => 'On',
 ];
