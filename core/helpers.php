c89900fd88ef3d5046ba4be4c952dabb5d104747
yangjunping <yangjunping@115.com>
2019-01-18 11:56:00 +0800

    客户端IP

--- core/helpers.php
+++ core/helpers.php
@@ -249,3 +249,33 @@ function redirect($url)
     header('Location: ' . $url);
     exit;
 }
+
+/**
+ * 客户端IP
+ * @return string
+ */
+function client_ip()
+{
+    $ip = '';
+    if (isset($_SERVER['HTTP_CDN_SRC_IP'])) {
+        $ip = $_SERVER['HTTP_CDN_SRC_IP'];
+    } elseif (isset($_SERVER['HTTP_CLIENT_IP'])
+              && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', trim($_SERVER['HTTP_CLIENT_IP']))) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+              && preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', trim($_SERVER['HTTP_X_FORWARDED_FOR']), $matches)) {
+        foreach ($matches[0] AS $xip) {
+            $xip = trim($xip);
+            if (filter_var($xip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) {
+                $ip = $xip;
+                break;
+            }
+        }
+    } elseif (isset($_SERVER['HTTP_X_REAL_IP'])) {
+        $ip = $_SERVER['HTTP_X_REAL_IP'];
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return filter_var(trim($ip), FILTER_VALIDATE_IP);
+}
