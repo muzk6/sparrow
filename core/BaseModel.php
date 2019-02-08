e414890038497ae8e38031ee2885ea7f491d50c0
muzk6 <muzk6x@gmail.com>
2019-02-08 22:08:27 +0800

    Model 改为单例 Model 公开两个数据库方法 db(), sdb()

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -7,8 +7,13 @@ namespace Core;
  * 模型基类
  * @package Core
  */
-abstract class BaseModel implements InstanceInterface, ShardingInterface
+abstract class BaseModel implements InstanceInterface
 {
+    /**
+     * @var static 单例对象
+     */
+    private static $instance = null;
+
     /**
      * @var string 分区名
      */
@@ -29,37 +34,54 @@ abstract class BaseModel implements InstanceInterface, ShardingInterface
     }
 
     /**
+     * 分区分表逻辑<br>
+     * 具体逻辑在子类中覆盖实现
+     * @param int|string $index 分区分表索引值
+     * @return static
+     */
+    protected function sharding($index)
+    {
+        return $this;
+    }
+
+    /**
      * @inheritdoc
      * @return static
      */
     public static function instance()
     {
-        return new static();
-    }
-
-    /**
-     * @inheritdoc
-     * @param int|string $shardKey
-     * @return $this|ShardingInterface
-     */
-    public function sharding($shardKey)
-    {
-        //todo 分区分表逻辑...
-
-        if ($this->database) {
-            $this->table = "{$this->database}.{$this->table}";
+        if (!self::$instance) {
+            self::$instance = new static();
         }
 
-        return $this;
+        return self::$instance;
     }
 
     /**
-     * 数据库对象<br>
-     * @return AppPDO|\PDO
+     * 模型数据库对象<br>
+     * @return AppPDO
      */
-    public function db()
+    public static function db()
     {
-        return db()->section($this->section)->table($this->table);
+        $instance = self::instance();
+        return db()->table($instance->table);
+    }
+
+    /**
+     * 模型分区分表数据库对象 ShardingDB
+     * @param int|string $index
+     * @return AppPDO
+     */
+    public static function sdb($index)
+    {
+        $instance = self::instance();
+        $instance->sharding($index);
+
+        if ($instance->database) {
+            $instance->table = "{$instance->database}.{$instance->table}";
+        }
+
+        return db()->section($instance->section)->table($instance->table);
     }
 
 }
