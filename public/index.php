13292c57b3858e408c7d5e2294307604c0ef34ac
yangjunping <yangjunping@115.com>
2019-02-11 17:08:51 +0800

    维护模式样式

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 
 // 维护模式检查
 if (is_file(dirname(__DIR__) . '/data/down')) {
-    http_response_code(403);
+    http_response_code(503);
+    echo '<p style="text-align:center;color:#B0BEC5;font-size:72px;font-weight:lighter;position:relative;top:40%;">Be right back.</p>';
     exit;
 }
 
