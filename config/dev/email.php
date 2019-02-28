c5879fef7aad5eecd3276891c105ec2e30c6bb2b
muzk6 <muzk6x@gmail.com>
2019-02-28 22:55:16 +0800

    del. 敏感配置

--- config/dev/email.php
+++ config/dev/email.php
@@ -8,7 +8,7 @@ return [
     'host' => 'smtp.qq.com',
     'port' => 465,
     'encryption' => 'ssl',
-    'user' => '2216069209@qq.com',
-    'passwd' => 'gvmhmdijjtafdhgf',
+    'user' => 'demo@qq.com',
+    'passwd' => 'demo',
     'name' => 'knf',
 ];
