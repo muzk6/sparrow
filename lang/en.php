3c6fa2392e79ae73e3b191dd53332a194905283c
muzk6 <muzk6x@gmail.com>
2019-03-23 21:59:19 +0800

    change. token默认过期时间改为2小时

--- lang/en.php
+++ lang/en.php
@@ -4,7 +4,7 @@ return [
     // 通用
     10001000 => 'Parameter error',
     10001001 => 'Illegal request',
-    10001002 => 'The page has expired. Please try again',
+    10001002 => 'The page has expired, please refresh',
 
     // 用户
     10002000 => 'Wrong account or password',
