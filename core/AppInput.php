562933ca38ae268b023ae6fc1e151072a977128b
muzk6 <muzk6x@gmail.com>
2019-04-27 23:39:55 +0800

    change. AppInput

--- core/AppInput.php
+++ core/AppInput.php
@@ -10,21 +10,48 @@ use Exception;
  */
 class AppInput
 {
+    private $error_msg = [
+        'require' => ':attribute不能为空',
+        'number' => ':attribute必须为数字',
+        'array' => ':attribute必须为数组',
+        'float' => ':attribute必须为浮点数',
+        'boolean' => ':attribute必须为布尔值',
+        'email' => ':attribute必须为正确的邮件地址',
+        'url' => ':attribute必须为正确的url格式',
+        'ip' => ':attribute必须为正确的ip地址',
+        'timestamp' => ':attribute必须为正确的时间戳格式',
+        'date' => ':attribute必须为正确的日期格式',
+        'regex' => ':attribute格式不正确',
+        'in' => ':attribute必须在:range内',
+        'notIn' => ':attribute必须不在:range内',
+        'between' => ':attribute必须在:1-:2范围内',
+        'notBetween' => ':attribute必须不在:1-:2范围内',
+        'max' => ':attribute最大值为:1',
+        'min' => ':attribute最小值为:1',
+        'length' => ':attribute长度必须为:1',
+        'confirm' => ':attribute和:1不一致',
+        'gt' => ':attribute必须大于:1',
+        'lt' => ':attribute必须小于:1',
+        'egt' => ':attribute必须大于等于:1',
+        'elt' => ':attribute必须小于等于:1',
+        'eq' => ':attribute必须等于:1',
+    ];
+
     /**
      * 取出指定字段值
      * @param array &$refBucket $this->pool的参数集
      * @param string $name 字段名
      * @param string $type 需要强转为指定的类型
-     * @param mixed $defaultOrCallback 默认值或回调函数
+     * @param mixed|null $default
+     * @param callable|null $callback
      * @return array [$val, $err]
      */
-    protected function single(array &$refBucket, string $name, string $type, $defaultOrCallback)
+    protected function single($value, string $name, string $type, $default = null, callable $callback = null)
     {
-        $val = isset($refBucket[$name]) ? trim(strval($refBucket[$name])) : null; // 请求里没有指定参数时则为null
-        $ret = [$val, null];
-        if (is_callable($defaultOrCallback)) {
+        $ret = [$value, null];
+        if (is_callable($callback)) {
             try {
-                $callValue = $defaultOrCallback($val, $name);
+                $callValue = $callback($value, $name);
                 is_null($callValue) || $ret[0] = $callValue;
             } catch (Exception $exception) {
                 $ret[1] = [
@@ -37,7 +64,7 @@ class AppInput
                 }
             }
         } else {
-            $ret[0] = is_null($val) ? $defaultOrCallback : $val;
+            $ret[0] = is_null($value) ? $default : $value;
         }
 
         switch ($type) {
@@ -153,7 +180,7 @@ class AppInput
      * @see input()
      *
      * @param string|array $columns 单个或多个字段
-     * @param mixed $defaultOrCallback 默认值或回调函数，$columns 为 array 时无效<br>
+     * @param mixed $defaultOrCallback 自定义回调函数，$columns 为 array 时无效<br>
      * 回调函数格式为 function ($val, $name) {}<br>
      * 有return: 以返回值为准 <br>
      * 无return: 字段值为用户输入值 <br>
@@ -213,4 +240,113 @@ class AppInput
         $ret = (count($groups) == 1 && $isSingle) ? $groups[0] : $this->flat($keys, $groups);
         return $ret;
     }
+
+    public function parse2(string $field, $rules, $default = null, callable $callback = null)
+    {
+        list($bucket, $name, $type) = $this->key2name($field);
+
+        $title = $name;
+        $value = isset($bucket[$name]) ? trim(strval($bucket[$name])) : null; // 请求里没有指定参数时则为null
+        $this->check($value, $rules, $title);
+
+        $this->single($value, $name, $type, $default, $callback);
+    }
+
+    private function check($rules, $fieldValue, $fieldTitle)
+    {
+        is_string($rules) && $rules = explode('|', $rules);
+        foreach ($rules as $k => $v) {
+            if (is_numeric($k)) { // 格式：['rule0', 'rule1']
+                $rule = $v;
+                $customMsg = null;
+            } else { // 格式：['rule0' => 'message0', 'rule1' => 'message1']
+                $rule = $k;
+                $customMsg = $v;
+            }
+
+            $ruleValue1 = '';
+            $ruleValue2 = '';
+            $ruleRange = '';
+            $ruleName = $rule;
+            $ruleReverse = false;
+
+            if ($rule[0] === '!') {
+                $ruleReverse = true;
+                $rule = ltrim($rule, '!');
+            }
+
+            if (strpos($rule, ':')) {
+                $ruleSplit = explode(':', $rule);
+                $ruleName = $ruleSplit[0];
+                $ruleRange = $ruleSplit[1];
+
+                $ruleValues = explode(',', $ruleSplit[1]);
+                $ruleValue1 = $ruleValues[0] ?? '';
+                $ruleValue2 = $ruleValues[1] ?? '';
+            }
+
+            if (!$this->checkResult($fieldValue, $ruleName, $ruleReverse, $ruleRange, $ruleValue1, $ruleValue2)) {
+                $msg = $customMsg ? $customMsg : $this->getMessage($fieldTitle, $rule);
+                return [false, $msg];
+            }
+        }
+
+        return [true, null];
+    }
+
+    private function checkResult($fieldValue, string $ruleName, bool $ruleReverse, string $ruleRange, string $ruleValue1, string $ruleValue2)
+    {
+        $ret = false;
+        switch ($ruleName) {
+            case 'req':
+            case 'require':
+                $ret = !is_null($fieldValue);
+                break;
+            case 'num':
+            case 'number':
+                $ret = filter_var($fieldValue, FILTER_SANITIZE_NUMBER_INT);
+                break;
+            case 'arr':
+            case 'array':
+                $ret = is_array($fieldValue);
+                break;
+            case 'float':
+                $ret = filter_var($fieldValue, FILTER_VALIDATE_FLOAT);
+                break;
+            case 'bool':
+            case 'boolean':
+                $ret = filter_var($fieldValue, FILTER_VALIDATE_BOOLEAN);
+                break;
+            case 'email':
+                $ret = filter_var($fieldValue, FILTER_VALIDATE_EMAIL);
+                break;
+            case 'url':
+                $ret = filter_var($fieldValue, FILTER_SANITIZE_URL);
+            case 'ip':
+                $ret = filter_var($fieldValue, FILTER_VALIDATE_IP);
+                break;
+            case 'timestamp':
+                $ret = strtotime(date('Y-m-d H:i:s', $fieldValue)) == $fieldValue;
+                break;
+            case 'date':
+                $ret = strtotime($fieldValue) ? $fieldValue : false;
+                break;
+            case 'in':
+                $ret = in_array($fieldValue, $ruleRange);
+                break;
+            default:
+                break;
+        }
+
+        return $ruleReverse ? !$ret : !!$ret;
+    }
+
+    private function getMessage(string $title, string $rule)
+    {
+        if (isset($this->error_msg[$error_key])) {
+            return str_replace([':attribute', ':range', ':1', ':2'], [$title, $range, $value1, $value2], $this->error_msg[$error_key]);
+        }
+        return false;
+    }
+
 }
