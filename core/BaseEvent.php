7222b023e51f17a07b0326b420594516ca28230f
muzk6 <muzk6x@gmail.com>
2019-06-12 19:10:03 +0800

    ShouldQueue Interface 改成 sendAsync

--- core/BaseEvent.php
+++ core/BaseEvent.php
@@ -20,22 +20,32 @@ abstract class BaseEvent
     public function send(...$params)
     {
         try {
-            if ($this instanceof ShouldQueue) { // 异步队列
-                app(Queue::class)->publish(static::class, $params);
-                return null;
-            } else {
-                $ref = new ReflectionClass(static::class);
-                $listenerParams = [];
-                foreach ($ref->getMethod('listen')->getParameters() as $handleParam) {
-                    $dependClassName = $handleParam->getClass();
-                    if ($dependClassName) {
-                        $listenerParams[] = AppContainer::get($dependClassName->getName());
-                    } else {
-                        $listenerParams[] = array_shift($params);
-                    }
+            $ref = new ReflectionClass(static::class);
+            $listenerParams = [];
+            foreach ($ref->getMethod('listen')->getParameters() as $handleParam) {
+                $dependClassName = $handleParam->getClass();
+                if ($dependClassName) {
+                    $listenerParams[] = AppContainer::get($dependClassName->getName());
+                } else {
+                    $listenerParams[] = array_shift($params);
                 }
-                return call_user_func([$this, 'listen'], ...$listenerParams);
             }
+            return call_user_func([$this, 'listen'], ...$listenerParams);
+        } catch (\ReflectionException $e) {
+            trigger_error($e->getMessage());
+        }
+    }
+
+    /**
+     * 异步版 send()
+     * @param mixed ...$params
+     * @return null
+     */
+    public function sendAsync(...$params)
+    {
+        try {
+            app(Queue::class)->publish(static::class, $params);
+            return null;
         } catch (\ReflectionException $e) {
             trigger_error($e->getMessage());
         }
