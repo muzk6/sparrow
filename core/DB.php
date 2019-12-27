8c60baa0b84d8c1f1f18de5f795cd0e1546b683f
muzk6 <muzk6x@gmail.com>
2019-12-27 18:01:46 +0800

    有状态 DBConnection

--- core/DB.php
+++ core/DB.php
@@ -198,3 +198,131 @@ class DB
     }
 
 }
+
+class DBConnection
+{
+    /**
+     * @var PDO
+     */
+    protected $connection;
+
+    /**
+     * @var bool 日志开关
+     */
+    protected $log = false;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->connection, $name], $arguments);
+    }
+
+    /**
+     * 前置勾子
+     * @param string $sql
+     * @return string
+     */
+    protected function before(string $sql)
+    {
+        $sql = trim($sql);
+        if ($this->log) {
+            logfile('statement', $sql, 'sql');
+        }
+
+        return $sql;
+    }
+
+    /**
+     * 日志开关
+     * @param bool $open
+     */
+    public function switchLog($open = true)
+    {
+        $this->log = $open;
+    }
+
+    /**
+     * 查询一行记录
+     * @param string $sql 原生 sql 语句
+     * @param array $binds 防注入的参数绑定
+     * @return array|false 无记录时返回 false
+     */
+    public function selectOne(string $sql, array $binds = [])
+    {
+        $sql = $this->before($sql);
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute($binds);
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * 查询多行记录
+     * @param string $sql 原生 sql 语句
+     * @param array $binds 防注入的参数绑定
+     * @return array 无记录时返回空数组 []
+     */
+    public function selectAll(string $sql, array $binds = [])
+    {
+        $sql = $this->before($sql);
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute($binds);
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * 插入记录
+     * @param string $sql 原生 sql 语句
+     * @param array $binds 防注入的参数绑定
+     * @return int 返回最后插入的主键ID, 失败时返回0
+     */
+    public function insert(string $sql, array $binds = [])
+    {
+        $sql = $this->before($sql);
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute($binds);
+
+        return intval($this->connection->lastInsertId());
+    }
+
+    /**
+     * 更新记录
+     * @param string $sql 原生 sql 语句
+     * @param array $binds 防注入的参数绑定
+     * @return int 被更新的行数，否则返回0(<b>注意：不要随便用来当判断条件</b>)
+     */
+    public function update(string $sql, array $binds = [])
+    {
+        $sql = $this->before($sql);
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute($binds);
+
+        return $statement->rowCount();
+    }
+
+    /**
+     * 删除记录
+     * @param string $sql 原生 sql 语句
+     * @param array $binds 防注入的参数绑定
+     * @return int 被删除的行数，否则返回0(<b>注意：不要随便用来当判断条件</b>)
+     */
+    public function delete(string $sql, array $binds = [])
+    {
+        $sql = $this->before($sql);
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute($binds);
+
+        return $statement->rowCount();
+    }
+
+}
--- public/test.php
+++ public/test.php
@@ -12,23 +12,29 @@ require_once dirname(__DIR__) . '/init.php';
 app(Whitelist::class)->checkSafeIpOrExit();
 
 //todo..
-//var_dump(db()->getConnection(0));exit;
-//db()->getConnection()->beginTransaction();
-//db()->getConnection()->beginTransaction();
-//db()->getConnection()->beginTransaction();
-//db()->getConnection()->commit();
-//exit;
+$conn = db()->beginTransaction();
+exit;
 $sqlAll = "select * from test";
 $sqlOne = "select * from test where id = :id";
 
-$ds['all'] = db()->getAll($sqlAll, [], false, 'sec0');
+$ds['all1'] = db()->getAll($sqlAll, [], false, 'sec0');
 $ds['insert'] = $id = db()->query("insert into test(name, `order`) values(?, ?)", ['tom_04', 3], 'sec0');
-$ds['one'] = db()->getOne($sqlOne, ['id' => $id]);
+$ds['one1'] = db()->getOne($sqlOne, ['id' => $id]);
 $ds['update'] = db()->query('update test set name = :name where id = :id', ['name' => 'tom_44', 'id' => $id]);
-$ds['one'] = db()->getOne($sqlOne, ['id' => $id]);
+$ds['one2'] = db()->getOne($sqlOne, ['id' => $id]);
 $ds['delete'] = db()->query('delete from test where id = :id', ['id' => $id], 'sec0');
 db()->close();
-$ds['all'] = db()->getAll($sqlAll);
+$ds['all2'] = db()->getAll($sqlAll);
+
+$update = [
+    'name' => 'tom_042',
+    'order' => 3,
+    'id' => 196
+];
+$sql = "update test set name=:name, `order`=:order where id=:id";
+db()->query($sql, $update);
+
+$ds['one3'] = db()->getOne('select * from test where id = :id', ['id' => 196]);
 
 var_dump($ds);
 exit;
