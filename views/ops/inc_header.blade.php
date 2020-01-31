de2b5957b3152f696001d53c295849e14f4c468f
muzk6 <muzk6x@gmail.com>
2020-01-31 15:48:37 +0800

    改名

--- views/ops/inc_header.blade.php
+++ views/ops/inc_header.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<link rel="stylesheet" href="/static/index.css">
+<link rel="stylesheet" href="/static/element-ui.css">
 <script src="/static/vue.js"></script>
-<script src="/static/index.js"></script>
+<script src="/static/element-ui.js"></script>
 <script src="/static/jquery.js"></script>
