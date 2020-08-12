ea36880f708a54e3336faf41b2fca5b09e4b6f1e
muzk6 <muzk6x@gmail.com>
2020-08-12 22:04:30 +0800

    发布 composer

--- config/dev/app.php
+++ config/dev/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'secret_key' => '8f037a11f3aa039e1cd48bfbc70ec893', // 密钥，每个项目必须不一样
+    'secret_key' => '8f037a11f3aa039e1cd48bfbc70ec893', // 密钥，建议不要和生产环境一样
     'csrf_token_expire' => 0, // csrf_token 过期时间(秒), 0 表示不过期
     'timezone' => 'PRC',
     'lang' => 'zh_CN',
