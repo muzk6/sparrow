e82b94b0cc53e65309be1397182c79bceb7d817b
muzk6 <muzk6x@gmail.com>
2020-08-13 14:52:13 +0800

    create_project.php

--- create_project.php
+++ create_project.php
@@ -12,3 +12,10 @@ $filename = 'app/Controllers/OPS/IndexController.php';
 $content = file_get_contents($filename);
 $content = str_replace('ops.sparrow', substr(uniqid (), -6), $content);
 file_put_contents($filename, $content);
+
+// 重置白名单 Cookie
+$filename = 'config/dev/whitelist.php';
+$content = file_get_contents($filename);
+$cookie = hash_pbkdf2 ('sha256', uniqid(), '5f34e2f3b697e', 1e3);
+$content = str_replace('e1c07665849e18b27f7a6d3e86894e8c657b628f5baccf637afc3d0a4a965a02', $cookie, $content);
+file_put_contents($filename, $content);
