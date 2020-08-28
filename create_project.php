00077ceaf40d3c3ec7164d576f223d62f7347d81
muzk6 <muzk6x@gmail.com>
2020-08-28 10:44:56 +0800

    脚本 重置运维后台密码

--- create_project.php
+++ create_project.php
@@ -8,7 +8,7 @@ $content = str_replace('8f037a11f3aa039e1cd48bfbc70ec893', $secretKey, $content)
 file_put_contents($filename, $content);
 
 // 重置运维后台密码
-$filename = 'app/Controllers/OPS/IndexController.php';
+$filename = 'app/Routes/OPS/index.php';
 $content = file_get_contents($filename);
 $content = str_replace('ops.sparrow', substr(uniqid (), -6), $content);
 file_put_contents($filename, $content);
