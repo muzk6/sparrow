56e763e93d220f23b422bd5a145b649e96621e9c
muzk6 <muzk6x@gmail.com>
2020-08-29 10:39:18 +0800

    完善备注

--- app/Routes/index.php
+++ app/Routes/index.php
@@ -6,11 +6,16 @@
 
 use Core\Router;
 
-// 前台主页
+/**
+ * 前台主页
+ */
 route_get('/', function () {
     return 'Hello Sparrow.<br><a href="/demo">>>>查看示例</a>';
 });
 
+/**
+ * Demo 示例
+ */
 route_group(function () {
     include PATH_ROUTES . '/demo.php';
 });
