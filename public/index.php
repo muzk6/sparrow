3c64684034764d4ece72cd111f3a8cd460c94716
muzk6 <muzk6x@gmail.com>
2020-09-12 11:29:01 +0800

    注释

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-// 维护模式检查
 use Core\Router;
 
+// 维护模式检查
 if (is_file(dirname(__DIR__) . '/data/down')) {
     http_response_code(503);
     echo '<p style="text-align:center;color:#B0BEC5;font-size:72px;font-weight:lighter;position:relative;top:40%;">Be right back.</p>';
